a90bf2520230d6763a1f426d832922d95b217394
SimonXz <thetrueslgaming@gmail.com>
2019-04-11 23:00:56 +0100

    Coding PrisonMain

--- src/main/java/prison/prison/PrisonMain.java
+++ src/main/java/prison/prison/PrisonMain.java
@@ -2,10 +2,20 @@ package prison.prison;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class PrisonMain extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getLogger().info("[Prison] Enabled");
+        File file = new File(getDataFolder(), "config.yml");
+        if (!file.exists()) {
+            getLogger().info("[Prison] Config not found, Creating one for you now!");
+            saveDefaultConfig();
+        } else {
+            getLogger().info("[Prison] Config found, Loading!");
+        }
         // Plugin startup logic
 
     }
